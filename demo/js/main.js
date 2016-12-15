409b7ecb54d358d9570645f8f0d2c8f46d2ed480
zmx2321@163.com <zmx2321@163.com>
2016-12-15 19:48:01 +0800

    gx

--- demo/js/main.js
+++ demo/js/main.js
@@ -1,6 +1,23 @@
 $(function(){
+	demo();
+	demo_contHeight();
     demo1();
 });
+
+//选项卡
+var demo = function() {
+    $('ul.demo_btn li').click(function(){
+        $(this).addClass('active').siblings().removeClass('active');
+        $('ul.demo_cont li').eq($(this).index()).show().siblings().hide();
+    });	
+}
+
+//给demo_cont高
+var demo_contHeight = function() {
+	var topHead = $(".demo_btn").height() + 50;
+	console.log(topHead);
+	$(".demo_cont").css("height", $(window).height() - topHead - 200);
+}
 
 // Swiper滚动插件 demo1
 var demo1 = function() {
