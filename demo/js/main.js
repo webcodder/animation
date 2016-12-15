c83938452e6ef0970fe86b558302110235df3536
zmx2321@163.com <zmx2321@163.com>
2016-12-15 18:56:41 +0800

    gx

--- demo/js/main.js
+++ demo/js/main.js
@@ -6,7 +6,7 @@ $(function(){
 var demo1 = function() {
     var swiper = new Swiper('.demo1 .swiper-container', {
         pagination: '.swiper-pagination',
-        slidesPerView: 2.6,
+        slidesPerView: 5.8,
         paginationClickable: true,
         spaceBetween: 10,
         freeMode: true
